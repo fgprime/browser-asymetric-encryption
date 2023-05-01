1ae76ca0f7368cbbc936ecaa378664bb968fab17
Franz Geiselbrechtinger <fg@primeinteractive.de>
2023-05-01 23:46:09 +0200

    Merge branch 'main'

--- pages/index.js
+++ pages/index.js
@@ -11,27 +11,74 @@ export default function Home() {
   const [encryption, setEncryption] = useState(false);
   const [cryptoKey, setCryptoKey] = useState(null);
 
-
   useEffect(() => {
     const creatCryptoKey = async () => {
       const publicKeyPem = await loadPublicKey();
       const result = await createCryptoKeyFromPublicKey(publicKeyPem);
       setCryptoKey(result);
-    }
+    };
     creatCryptoKey();
+
+    example();
   }, []);
 
-  const loadPublicKey = async () => {
-    const response = await fetch("publickey");
+  const example = async () => {
+    // Load external public key from PEM file
+    const publicKeyPem = await loadPublicKey();
+
+    // Create crypto key from public key PEM file
+    const cryptoKeyPublic = await createCryptoKeyFromPublicKey(publicKeyPem);
+
+    // Encrypt message based on public key
+    const encodedCipher = await encrypt(MESSAGE, cryptoKeyPublic);
+    console.log(encodedCipher);
+
+    // Load internal example private key from PEM const
+    const privateKeyPem = process.env.privateKey;
+
+    // Create crypto key from private key PEM file
+    const cryptoKeyPrivate = await createCryptoKeyFromPrivateKey(privateKeyPem);
+
+    // Decrypt message based on private key
+    const message = await decrypt(encodedCipher, cryptoKeyPrivate);
+
+    console.dir(MESSAGE);
+    console.dir(message);
+  };
+
+  const encryptEvent = async () => {
+    const cipherArray = await encrypt(message, cryptoKey);
+
+    setCiphertext(cipherArray);
+    setEncryption(true);
+    setMessage(cipherArray);
+  };
+
+  const decryptEvent = async () => {
     try {
-      if (!response.ok) {
-        throw new Error("Network response failed");
-      }
-      return response.text();
+      const key = await createCryptoKeyFromPrivateKey(process.env.privateKey);
+      const message = await decrypt(ciphertext, key);
+      setEncryption(false);
+      setMessage(message);
     } catch (error) {
       console.error(error);
     }
+  };
 
+  const loadPublicKey = async () => {
+    try {
+      const response = await fetch("publickey");
+
+      if (!response.ok) {
+        throw new Error("Network response failed");
+      }
+
+      return response.text();
+    } catch (error) {
+      console.log(error);
+
+      return;
+    }
   };
 
   const createCryptoKeyFromPublicKey = (publicKeyPem) => {
@@ -47,54 +94,52 @@ export default function Home() {
     );
   };
 
-  const encryptMessage = async (message, key) => {
-    const encoder = new TextEncoder();
-    const encoded = encoder.encode(message);
-
-    const cipherArray = await window.crypto.subtle.encrypt(
-      { name: "RSA-OAEP" },
-      key,
-      encoded
+  const createCryptoKeyFromPrivateKey = (privateKeyPem) => {
+    return crypto.subtle.importKey(
+      "pkcs8",
+      pemToBinary(privateKeyPem),
+      {
+        name: "RSA-OAEP",
+        hash: "SHA-256",
+      },
+      false,
+      ["decrypt"]
     );
-    setCiphertext(cipherArray);
-
-    return Uint8Array.from(cipherArray.toString(), (c) => c.charCodeAt(0)).join('')
-
   };
 
-  const decryptMessage = async (key) => {
-    const originalText = await window.crypto.subtle.decrypt(
+  const encrypt = async (message, cryptoKeyPublic) => {
+    let encoder = new TextEncoder();
+    let encoded = encoder.encode(message);
+
+    const cipher = await window.crypto.subtle.encrypt(
+      { name: "RSA-OAEP" },
+      cryptoKeyPublic,
+      encoded
+    );
+
+    return arrayBufferToBase64(cipher);
+  };
+
+  const decrypt = async (encodedCipher, cryptoKeyPrivate) => {
+    const cipher = base64ToArrayBuffer(encodedCipher);
+
+    let message = await window.crypto.subtle.decrypt(
       {
         name: "RSA-OAEP",
       },
-      key,
-      ciphertext
+      cryptoKeyPrivate,
+      cipher
     );
 
     const decoder = new TextDecoder();
-    return decoder.decode(originalText);
+
+    return decoder.decode(message);
   };
 
-  const encrypt = async () => {
-    const cipher = await encryptMessage(message, cryptoKey);
-    setEncryption(true);
-    setMessage(cipher);
-  };
-
-  const decrypt = async () => {
-    try {
-      const key = await privateKey();
-      const message = await decryptMessage(key);
-      setEncryption(false);
-      setMessage(message);
-    } catch (error) {
-      console.error(error)
-    }
-  };
-
+  // Convert PEM key file to a binary key
   const pemToBinary = (pem) => {
     const lines = pem.split("\n");
-  
+
     const start = lines.findIndex((element) => {
       return element.includes("BEGIN") && element.includes("KEY");
     });
@@ -107,24 +152,31 @@ export default function Home() {
 
     const encoded = lines
       .filter((_, index) => index > start && index < end)
-      .join("").replace(/\s/g, "");
+      .join("")
+      .replace(/\s/g, "");
 
     return Uint8Array.from(window.atob(encoded), (c) => c.charCodeAt(0));
   };
 
-  // Remove from production code
-  const privateKey = () => {
-    return crypto.subtle.importKey(
-      "pkcs8",
-      pemToBinary(process.env.privateKey),
-      {
-        name: "RSA-OAEP",
-        hash: "SHA-256",
-      },
-      false,
-      ["decrypt"]
-    );
-  };
+  function arrayBufferToBase64(buffer) {
+    var binary = "";
+    var bytes = new Uint8Array(buffer);
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+  }
+
+  function base64ToArrayBuffer(base64) {
+    var binary_string = window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+      bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
+  }
 
   return (
     <div className={styles.container}>
@@ -136,8 +188,19 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1>En/Decryption example</h1>
-        <p><input type="text" value={message} readOnly={encryption} onChange={(e) => setMessage(e.target.value)} /></p>
-        {encryption ? <button onClick={() => decrypt()}>Decrypt </button> : <button onClick={() => encrypt()}>Encrypt</button>}
+        <p>
+          <input
+            type="text"
+            value={message}
+            readOnly={encryption}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </p>
+        {encryption ? (
+          <button onClick={() => decryptEvent()}>Decrypt </button>
+        ) : (
+          <button onClick={() => encryptEvent()}>Encrypt</button>
+        )}
       </main>
     </div>
   );
